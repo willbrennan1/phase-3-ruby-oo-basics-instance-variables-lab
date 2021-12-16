5012d6fa01213c59434027b177a278312a60f7ee
Will Brennan <willbrennan314@gmail.com>
2021-12-16 08:39:12 -0500

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
 
+    #setter method
+    def name=(dog_name)
+        # assign the @name instance variable a value of whateveer is passed as an argument
+        @this_dogs_name = dog_name    # An @ means it is an instance variable and is able to be calledo outside
+    end
+
+    def name
+        @this_dogs_name 
+    end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+lassie.name
